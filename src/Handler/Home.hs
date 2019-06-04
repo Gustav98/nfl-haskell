a4c585642c0e95516b08bcccdb243ededa018112
Gustavo Oliveira <gustavo.oliveira141998@hotmail.com>
2019-06-04 22:28:09 +0000

    ajustes home

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -6,8 +6,15 @@
 module Handler.Home where
 
 import Import
-import Network.HTTP.Types.Status
+
 import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
-getHomeR = undefined
+getHomeR = do
+    defaultLayout $ do 
+        addStylesheet $ StaticR css_style_css
+        addStylesheet $ StaticR css_bootstrap_css
+        addStylesheetRemote "https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        $(whamletFile "templates/home.hamlet")
+        
+        
